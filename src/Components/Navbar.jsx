e48b053357fa0cd11cd4ec45dd63c38ebcfb92b4
Federico Sabiche <fsabiche>
2023-10-17 18:41:08 -0300

    se modifico y se corrigio el menu burger

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -993,7 +993,7 @@ const Navbar = () => {
                 </span>
               </button>
               {activeDropdown === "aboutDropdownOpen" && (
-                <ul className="ml-4 mb-2 absolute left-0 mt-2 w-auto bg-white border border-gray-300 shadow-lg">
+                <ul className="ml-4 mb-2 absolute left-0 mt-2 w-auto bg-white border border-gray-300 shadow-lg z-10">
                   <li>
                     <Link
                       to="/Nosotros"
@@ -1071,7 +1071,7 @@ const Navbar = () => {
                 </span>
               </button>
               {activeDropdown === "aboutDropdownOpen2" && (
-                <ul className="ml-4 mb-2 absolute left-0 mt-2 w-auto bg-white border border-gray-300 shadow-lg">
+                <ul className="ml-4 mb-2 absolute left-0 mt-2 w-auto bg-white border border-gray-300 shadow-lg z-10">
                   <li>
                     <Link
                       to="/Tecningenio"
